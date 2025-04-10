82fadd58bf8a63466dd887f0ba8a3472d6bbf4bf
Chanon Eiemrod <chanoneiemrod@Chanons-MacBook-Air.local>
2025-04-10 10:57:54 +0700

    chore: add return type

--- promotion/member_promotion.go
+++ promotion/member_promotion.go
@@ -16,8 +16,8 @@ func (p *memberPromotion) ValidatePromotion(orders *order.Orders) bool {
 	return orders.IsMember()
 }
 
-func (p *memberPromotion) GetDiscount(orders *order.Orders) *big.Float {
+func (p *memberPromotion) GetDiscount(orders *order.Orders) (discount *big.Float) {
 	total := orders.GetTotal()
-	discount := new(big.Float).Mul(total, big.NewFloat(0.1))
+	discount = new(big.Float).Mul(total, big.NewFloat(0.1))
 	return discount
 }
